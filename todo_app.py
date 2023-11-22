e8ea03488783501bb248c3c363827d408f58b8b1
Mad-AtomiC <129085001+Mad-AtomiC@users.noreply.github.com>
2023-11-22 21:28:04 +0600

    Fixed icon path

--- todo_app.py
+++ todo_app.py
@@ -34,7 +34,7 @@ todo.config(bg='#1E1F21')
 todo.resizable(False, False)
 
 # setting app icon
-icon_path = "Todo-App/image/todo.ico"
+icon_path = "image/todo.ico"
 todo.iconbitmap(icon_path)
 
 # labels
